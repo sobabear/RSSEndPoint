b1d976185ffcbc4fe0e399575f8f3aac0d050fe3
yongjun <stareta1202@gmail.com>
2025-07-04 01:31:46 +0900

    Add db setting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { RssModule } from './modules/rss/rss.module';
+import { databaseConfig } from './config/database.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(databaseConfig),
+    RssModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
