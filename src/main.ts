893b928c278a61c57eea77d0d10ec2ec407f3d52
yongjun <stareta1202@gmail.com>
2025-07-04 14:13:44 +0900

    Add global prefix

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('rss');
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(process.env.PORT ?? 3020);
 }
